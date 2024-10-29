c7813da4a8b4a107b3f08855c138b1a12c020cba
dhairyaaa15 <dhairyaitm123@gmail.com>
2024-10-29 21:08:34 +0530

    additional test

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.use(express.json());
 app.use('/api/customers', customerRoutes);
 app.use('/api/items', itemRoutes);
 app.use('/api/maintenance', maintenanceRoutes);
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+  });
+  
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
