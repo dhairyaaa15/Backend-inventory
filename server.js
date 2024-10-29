1aedd54ea6432eafe5a385ac1acdfe0c0288028b
dhairyaaa15 <dhairyaitm123@gmail.com>
2024-10-30 01:54:40 +0530

    port changed-5000

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ app.get('/health', (req, res) => {
   });
   
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
