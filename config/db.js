7f602a0f3ee35503bb328d821dfabb36a6f6896a
dhairyaaa15 <dhairyaitm123@gmail.com>
2024-10-30 22:14:22 +0530

    new changes

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://dhairyaa15:practicehard@127.0.0.1:27017/inventoryDB"); // No options needed
+    await mongoose.connect("mongodb+srv://dhairyaitm123:SSguVtPdwd15ba2N@cluster0.fnnj9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"); // No options needed
     console.log('MongoDB Connected');
   } catch (error) {
     console.error(error.message);
