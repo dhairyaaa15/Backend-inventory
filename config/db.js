b4d44a4a75cc2517260399b3f8ab73e9f940c668
dhairyaaa15 <dhairyaitm123@gmail.com>
2024-11-05 13:37:50 +0530

    uri update

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://dhairyaitm123:SSguVtPdwd15ba2N@cluster0.fnnj9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"); // No options needed
+    await mongoose.connect(MONGO_URI); // No options needed
     console.log('MongoDB Connected');
   } catch (error) {
     console.error(error.message);
